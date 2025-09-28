1e0cbd20e80d61fbde5787e3613198a41ab97c55
David Liu <davidliu@Davids-Macbook-Air-3.local>
2025-09-28 14:29:34 -0400

    try 2

---
+++ Game Jam/Assets/scripts/LogicManagerScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LogicManagerScript : MonoBehaviour
+{
+    public int PlayerScore;
+    public Text scoreText;
+    public GameObject GameOverScreen;
+    private float Timer = 0f;
+    private bool isAlive = true;
+    void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer >= 1f && isAlive)
+        {
+            PlayerScore += 1;
+            Timer = 0f;
+
+            scoreText.text = PlayerScore.ToString();
+        }
+
+    }
+
+    public void restartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void GameOver()
+    {
+        isAlive = false;
+        GameOverScreen.SetActive(true);
+    }
+}
--- Game Jam/Assets/scripts/playerInputScript.cs
+++ Game Jam/Assets/scripts/playerInputScript.cs
@@ -6,22 +6,31 @@ using UnityEngine;
 public class playerInputScript : MonoBehaviour
 {
     public Rigidbody2D playerBody;
-
+    public LogicManagerScript logicManager;
     public float upStrength = 1;
+    private bool isAlive =  true;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        logicManager =  GameObject.FindGameObjectWithTag("logic").GetComponent<LogicManagerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey == true)
+        if (Input.anyKey == true && isAlive == true)
         {
             playerBody.velocity = Vector2.up * upStrength;
         }
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        logicManager.GameOver();
+        isAlive = false;
     }
 
     
